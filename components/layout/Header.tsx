1a88cfb1b821a71b019dacbc3dc06988f701b72b
Darian Roncevic <darian@heliozgroup.com>
2026-02-24 13:27:48 +0100

    Mobile header fix

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -71,13 +71,13 @@ export default function Header() {
           >
             Projects
           </Link>
-          <Link
+          {/* <Link
             href="#experience"
             className="hover:underline underline-offset-4 transition"
             onClick={() => setMenuOpen(false)}
           >
             Experience
-          </Link>
+          </Link> */}
           <Link
             href="#journey"
             className="hover:underline underline-offset-4 transition"
